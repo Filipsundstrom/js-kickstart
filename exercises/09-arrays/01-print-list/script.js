f9c82c52fbbb441107f11a3bc5db3748e84747c5
Filipsundstrom <filip.sundstrom@elev.boden.se>
2025-09-11 13:29:36 +0200

    .

--- exercises/09-arrays/01-print-list/script.js
+++ exercises/09-arrays/01-print-list/script.js
@@ -3,14 +3,18 @@
 
 // TODO: Create an array with at least 5 string items
 // Example: let items = ["apple", "banana", "orange", "grape", "kiwi"];
+let fruits = ["apple", "banana", "kiwi","orange","grape" ];
+
 
 // TODO: Use a for loop to go through each item in the array
 // Remember: arrays start at index 0, but numbering should start at 1
-
+for (let i = 0; i < fruits.length; i++) {
+    console.log((i + 1) + ". " + fruits[i]);
+}
 // TODO: Print each item with a number prefix (1. item, 2. item, etc.)
 // Example: console.log((i + 1) + ". " + items[i]);
 
 // TODO: Display the total count at the end
 // Example: console.log("Total items: " + items.length);
 
-console.log("Replace this with your solution!");
+console.log("Total items: " + fruits.length);
